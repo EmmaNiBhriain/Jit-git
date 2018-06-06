1f19e331b346f07c06052e90205252e06e850d26
Emma <007eob@gmail.com>
2018-06-06 11:39:52 +0200

    FileNode object class has been created to hold the contents of a file as well as the hash of the file. This will be used to create a Merkle tree. A start has also been made on writing the test for the add() method in the Jit class

---
+++ src/main/java/de/othr/ajp/FileNode.java
@@ -0,0 +1,12 @@
+package de.othr.ajp;
+
+import java.io.Serializable;
+
+public class File extends Serializable{
+
+    private File leftChild;
+    private File rightChild;
+
+    private long hashValue;
+
+}
